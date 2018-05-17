a9e16075641fedbdbab3531cde9d6a3d4c1aada5
Nikita Zamiatin <4406015@gmail.com>
2018-05-17 15:02:02 -0400

    start with transaction

--- controller/accounts.js
+++ controller/accounts.js
@@ -5,7 +5,10 @@ const index = (request , response)=>{
     const account = accountModel.index();
     response.send(account)
 }
-const show = (request, response) => {}
+const show = (request, response) => {
+        const account = accountModel.show(request.params.id);
+        response.send(account)
+}
 const create = (request, response) => {}
 const update = (request, response) => {}
 const destroy = (request, response) => {}
--- models/account.js
+++ models/account.js
@@ -1,13 +1,18 @@
 const fs = require('fs');
 const path = require('path');
-const accountPath = path.join(__dirname, ".." ,"data", "accounts.json")
+const accountPath = path.join(__dirname, "..", "data", "accounts.json")
 
-const index = () =>{
+const index = () => {
     const accountJSON = fs.readFileSync(accountPath, 'utf-8');
     const accounts = JSON.parse(accountJSON);
     return accounts
 }
-const show = () => {}
+const show = (id) => {
+    const accountJSON = fs.readFileSync(accountPath, 'utf-8');
+    const accounts = JSON.parse(accountJSON);
+    const accountFound = accounts.find(account => account.id === id);
+    return accountFound
+}
 const create = () => {}
 const update = () => {}
 const destroy = () => {}
